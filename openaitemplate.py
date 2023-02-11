3d986a1127785622e2df38db836c9bd26492273f
GeoffreyFClark <97141856+GeoffreyFClark@users.noreply.github.com>
2023-02-11 15:02:20 -0700

    Add files via upload

---
+++ openaitemplate.py
@@ -0,0 +1,35 @@
+import discord
+import openai
+
+openai.api_key = "OPEN AI API KEY HERE"  # Obtain from openAI platform
+
+intents = discord.Intents().all()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'SUCCESSFULLY logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('!'):
+        prompt = message.content[1:]
+        response = openai.Completion.create(
+            engine="text-davinci-002",  # Select a base engine option or your own fine-tuned model here
+            prompt=prompt,
+            max_tokens=80,
+            n=1,
+            temperature=0.5,
+        )
+        generated_text = response["choices"][0]["text"]
+
+        # Add further logic here to manipulate string responses if required.
+
+        await message.channel.send(f'{generated_text}')
+
+client.run("DISCORD BOT TOKEN HERE")  # Obtain from Discord Developer Portal
+
+
+# To fine tune your own model using openAI's API, see https://platform.openai.com/docs/guides/fine-tuning
