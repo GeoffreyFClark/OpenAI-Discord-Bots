57373b724347ce8fca5cacfa8f5143177184ae55
GeoffreyFClark <97141856+GeoffreyFClark@users.noreply.github.com>
2023-03-05 13:27:37 -0700

    update

--- tensorflow_trainyourmodel.py
+++ tensorflow_trainyourmodel.py
@@ -1,15 +1,39 @@
+import discord
 import tensorflow as tf
+import numpy as np
 
-# Build and train the model
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(10, activation='relu', input_shape=(5,)),
-    tf.keras.layers.Dense(1)
-])
-model.compile(optimizer='adam', loss='mse')
-model.fit(X_train, y_train, epochs=10)  # Where X_Train and Y_Train are input and output
+# Load the pre-trained model
+model = tf.keras.models.load_model("model.h5")
 
-# Save the trained model to a HDF5 file
-model.save('my_model.h5')
+# Function to generate answers
+def generate_answer(input_text, model):
+    input_seq = np.zeros((1, max_len), dtype=int)
+    for i, word in enumerate(input_text.split()):
+        if word in word_index:
+            input_seq[0, i] = word_index[word]
+    chat_response = model.predict(input_seq, verbose=0)[0]
+    chat_response = np.argmax(chat_response)
+    response = ''
+    for word, index in word_index.items():
+        if index == chat_response:
+            response = word
+            break
+    return response
 
-# Load the saved model
-loaded_model = tf.keras.models.load_model('my_model.h5')
+# Discord client
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    input_text = message.content
+    response = generate_answer(input_text, model)
+    await message.channel.send(response)
+
+client.run("DISCORD BOT TOKEN HERE")  # Obtain from Discord Developer Portal
